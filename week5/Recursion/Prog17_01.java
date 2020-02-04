0779109a6e8913e5a1b8bf453cfce23780a6314d
Manuel Garcia <mgarc881@fiu.edu>
2020-02-04 13:41:41 -0500

    2/4/2020

--- week5/Recursion/Prog17_01.java
+++ week5/Recursion/Prog17_01.java
@@ -3,11 +3,15 @@ public class Prog17_01{
 
 public Prog17_01(){
   System.out.println(" 4 factorial equals= "+ factorial(125));
-
   System.out.println(" recurrense of 4 = "+recurrense(4) );
+
+  System.out.println(" fibonacci number of 8= "+fibonacci(8));
+  System.out.println(" fibonacci number of 21= "+fibonacci(21));
+
+
+  int [] arr = {2,6,7,8};
+  printBackwards(arr, arr.length);
 }
-
-
 /**
  * @param num any number
  * @return the factorial of the number 
@@ -19,7 +23,6 @@ long factorial(int num){
   }
 }
 /**
- * 
  * @param num any number
  * @return returns the recurrense of that number
  */
@@ -32,9 +35,36 @@ public double recurrense(double num){
   }else{
     return (2*recurrense(num-1))+3;
   }
-
 }
 
+/**
+ * @param num any number
+ * @return the fibonacci number of the entered number
+ */
+public double fibonacci(double num){
+  if(num == 0){
+    return 0;
+  }else if(num==1){
+    return 1;
+  }else{
+    return fibonacci(num-1) + fibonacci(num-2);
+  }
+
+}
+/**
+ * method that prints an array backwards
+ */
+public void printBackwards(int []arr, int length){
+  
+  if(length == 1){
+    System.out.println(arr[0]+ " DONE PRINTING");
+  }else if( length > 1){
+
+   System.out.println(" "+ arr[length-1]+ " ");
+   printBackwards(arr, length -1);
+  }
+   
+}
 
 public static void main(String[]args){
 
