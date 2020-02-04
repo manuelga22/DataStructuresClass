dab1568a2e323d0b1d5b102b92279e1a67d497ea
Manuel Garcia <mgarc881@fiu.edu>
2020-02-04 13:15:31 -0500

    2/4/2020

--- week5/Recursion/Prog17_01.java
+++ week5/Recursion/Prog17_01.java
@@ -2,18 +2,37 @@
 public class Prog17_01{
 
 public Prog17_01(){
-  System.out.println(" 4 factorial equals= "+ factorial(4));
+  System.out.println(" 4 factorial equals= "+ factorial(125));
 
+  System.out.println(" recurrense of 4 = "+recurrense(4) );
 }
+
+
 /**
  * @param num any number
  * @return the factorial of the number 
  */
-public int factorial(int num){
+long factorial(int num){
   if(num==1) return 1;
   else{
     return num*factorial(num-1);
   }
+}
+/**
+ * 
+ * @param num any number
+ * @return returns the recurrense of that number
+ */
+public double recurrense(double num){
+  if(num<0){
+     return -1;
+  }
+  if (num ==0){
+    return 5;
+  }else{
+    return (2*recurrense(num-1))+3;
+  }
+
 }
 
 
