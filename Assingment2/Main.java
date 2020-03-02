4eeab2481da5360d9e302822dbfb8a5f7d4b80d6
Manuel Garcia <manuelisaacgarcia@gmail.com>
2020-03-01 18:31:04 -0800

    deque class

---
+++ Assingment2/Main.java
@@ -0,0 +1,113 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Tester class.
+ *
+ * @author Prof. A. Hernandez
+ */
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        new Main();
+    }
+
+    /**
+     * Tester method.
+     */
+    public Main()
+    {
+        Deque deque = new Deque();
+
+        File file = new File("assignment 2 exercise 1 test set.txt");
+
+        try
+        {
+            Scanner in = new Scanner(file);
+
+            String operation;
+            int item = 0;
+            int entryNumber = 0;
+            while (in.hasNextLine())
+            {
+                entryNumber++;
+                operation = in.next();
+                if (operation.equals("ADD_TO_BACK") || operation.equals("ADD_TO_FRONT"))
+                {
+                    item = in.nextInt();
+                    System.out.println("\n" + operation + " " + item);
+                }
+                else
+                    System.out.println("\n" + operation);
+
+                DequeItem result;
+                switch (operation)
+                {
+                    case "ADD_TO_BACK":
+                        if (deque.addToBack(item))
+                            System.out.println(deque);
+                        else
+                            System.out.println("Cannot add, deque is full!");
+                        break;
+
+                    case "ADD_TO_FRONT":
+                        if (deque.addToFront(item))
+                            System.out.println(deque);
+                        else
+                            System.out.println("Cannot add, deque is full!");
+                        break;
+
+                    case "GET_BACK":
+                        result = deque.getBack();
+                        if (result.valid)
+                            System.out.println("Back item: " + result.item);
+                        else
+                            System.out.println("Cannot retrieve value, deque is empty!");
+                        break;
+
+                    case "GET_FRONT":
+                        result = deque.getFront();
+                        if (result.valid)
+                            System.out.println("Front item: " + result.item);
+                        else
+                            System.out.println("Cannot retrieve value, deque is empty!");
+                        break;
+
+                    case "IS_EMPTY":
+                        System.out.println(deque.isEmpty());
+                        break;
+
+                    case "IS_FULL":
+                        System.out.println(deque.isFull());
+                        break;
+
+                    case "REMOVE_BACK":
+                        if (deque.removeBack())
+                            System.out.println(deque);
+                        else
+                            System.out.println("Cannot remove, deque is empty!");
+                        break;
+
+                    case "REMOVE_FRONT":
+                        if (deque.removeFront())
+                            System.out.println(deque);
+                        else
+                            System.out.println("Cannot remove, deque is empty!");
+                        break;
+
+                    default:
+                        System.out.println("Operation \"" + operation + "\" unknown at line " + entryNumber);
+                        System.exit(1);
+                }
+            }
+        } catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!");
+            System.exit(1);
+        }
+    }
+}
